a8fa26e0d191735094906adcaa90745c1097fec7
William9912 <2358634245@qq.com>
2022-08-06 21:08:23 +0800

    add response

--- internal/routers/api/v1/article.go
+++ internal/routers/api/v1/article.go
@@ -1,6 +1,11 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"blog-service/pkg/app"
+	"blog-service/pkg/errcode"
+
+	"github.com/gin-gonic/gin"
+)
 
 type Article struct{}
 
@@ -8,7 +13,14 @@ func NewArticle() Article {
 	return Article{}
 }
 
-func (a Article) Get(c *gin.Context)    {}
+func (a Article) Get(c *gin.Context) {
+	//下列函数返回的是gin.Context.H 本质上和直接
+	// r.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{"message": "pong"})
+	// })
+	//没什么区别 就是套了一层
+	app.NewResponse(c).ToErrorResponse(errcode.ServerError)
+}
 func (a Article) List(c *gin.Context)   {}
 func (a Article) Create(c *gin.Context) {}
 func (a Article) Update(c *gin.Context) {}

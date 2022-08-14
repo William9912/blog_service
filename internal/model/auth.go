7f0276e0b12bb0bac3a80727238943aa1012bcc7
William9912 <2358634245@qq.com>
2022-08-14 18:34:03 +0800

    1 2 3 and rebase 4 5
    123 and rebase 45

--- internal/model/auth.go
+++ internal/model/auth.go
@@ -12,6 +12,10 @@ func (a Auth) TableName() string {
 	return "blog_auth"
 }
 
+//解释一下这个表的作用，其实就是需要这个表里有你的发过来的内容，才会给你token 给你
+//token之后 你才能使用功能呢 至于表里怎么有发过来的内容 估计是注册的时候插入到数据库的
+//2 git rebase test
+//3 git rebase test
 //git rebase test 4
 //git rebase test 5
 func (a Auth) Get(db *gorm.DB) (Auth, error) {

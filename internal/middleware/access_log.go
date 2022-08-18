3bd946d87e9f5e29221aa1c29f53d4cc595915c6
William9912 <2358634245@qq.com>
2022-08-18 23:55:50 +0800

    add eamin & service information store

--- internal/middleware/access_log.go
+++ internal/middleware/access_log.go
@@ -9,18 +9,22 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+//一个gin.ResponseWriter的替代品（实现了这个接口）
 type AccessLogWriter struct {
 	gin.ResponseWriter
 	body *bytes.Buffer
 }
 
+//把p的内容写到buffer里
 func (w AccessLogWriter) Write(p []byte) (int, error) {
 	if n, err := w.body.Write(p); err != nil {
 		return n, err
 	}
+	//然后再用gin.ResponseWriter里的
 	return w.ResponseWriter.Write(p)
 }
 
+//一个日志中间件
 func AccessLog() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		bodyWriter := &AccessLogWriter{body: bytes.NewBufferString(""), ResponseWriter: c.Writer}
@@ -30,6 +34,7 @@ func AccessLog() gin.HandlerFunc {
 		c.Next()
 		endTime := time.Now().Unix()
 
+		//这个是已经执行完请求了
 		fields := logger.Fields{
 			"request":  c.Request.PostForm.Encode(),
 			"response": bodyWriter.body.String(),
@@ -42,3 +47,17 @@ func AccessLog() gin.HandlerFunc {
 		)
 	}
 }
+
+// //panic中间件
+// func Recovery() gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		defer func() {
+// 			if err := recover(); err != nil {
+// 				global.Logger.WithCallersFrames().Errorf("panic recover err: %v", err)
+// 				app.NewResponse(c).ToErrorResponse(errcode.ServerError)
+// 				c.Abort()
+// 			}
+// 		}()
+// 		c.Next()
+// 	}
+// }
